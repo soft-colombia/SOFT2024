82a338cf774bd03d62e9be0bb9b435af805a6aca
soft-colombia <softcolombia2024@gmail.com>
2024-10-22 15:34:10 -0500

    sin limpiar

--- codigo.js
+++ codigo.js
@@ -1,15 +1,1 @@
-document.getElementById('formulario').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita el comportamiento predeterminado de submit
-    const form = event.target;
-    
-    fetch(form.action, {
-        method: 'POST',
-        body: new FormData(form)
-    }).then(response => {
-        if (response.ok) {
-            form.reset(); // Limpia el formulario si el envío es exitoso
-        }
-    }).catch(error => {
-        console.error("Error al enviar el formulario:", error);
-    });
-});
+
